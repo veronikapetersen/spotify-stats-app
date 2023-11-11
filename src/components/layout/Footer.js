b5f41f7c97b92f635b0d42963deddf041f71feb0
Veronika P <karavannika@gmail.com>
2023-11-11 18:17:52 +0100

    footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,3 +1,5 @@
+
+import Link from 'next/link'
 import classes from "./Footer.module.scss";
 
 export default function Footer() {
@@ -6,10 +8,19 @@ export default function Footer() {
         <>
             <div className={classes.placeholder}></div>
             <footer className={classes.footer} >
-                <div>a</div>
-                <div>b</div>
-                <div>c</div>
-                <div>d</div>
+                <div className={classes['footer-content']}>
+                    <div className={classes['contact-info']}>
+                        <Link href="https://www.linkedin.com/in/yourname" target="_blank" rel="noopener noreferrer">LinkedIn</Link>
+                        <Link href="https://github.com/yourusername/your-app-repo" target="_blank" rel="noopener noreferrer">GitHub</Link>
+                        <Link href="mailto:your.email@example.com">Email</Link>
+                        <Link href="https://www.yourportfolio.com" target="_blank" rel="noopener noreferrer">Portfolio</Link>
+                        <Link href="https://www.buymeacoffee.com/veronikaaa">Buy me a coffee</Link>
+                    </div>
+                </div>
+
+                <div className={classes['copyright-info']}>
+                    <p>&copy; 2023 Veronika Petersen</p>
+                </div>
             </footer >
         </>
     )
